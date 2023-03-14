c1af53496b2efce5698c9873d9b1cb59ae3e1a40
vlezya <vlezyashop@gmail.com>
2023-03-14 22:32:36 +0200

    Added one-to-many association between users and and articles from backend

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
       redirect_to @article, notice: t('flash.article.create.notice')
     else
