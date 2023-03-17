bdde6d7fbcfdcc85d2e04f2af18d6da0577b34cf
vlezya <vlezyashop@gmail.com>
2023-03-17 17:48:10 +0200

    Add login view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   root 'home#page'
   resources :articles
-  resources :users
-
   get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
