4cfa2b9003c7ac2140a2089dd3f6ca9a99c9ab91
vlezya <vlezyashop@gmail.com>
2023-03-18 18:08:33 +0200

    Add contoller helper methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       redirect_to @article, notice: t('flash.article.create.notice')
     else
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def page
+    redirect_to articles_path if logged_in?
   end
 end
