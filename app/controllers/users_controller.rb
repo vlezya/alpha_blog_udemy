b90e91e7cfdc6743c5bdb1f476d2df24ecfcf438
vlezya <vlezyashop@gmail.com>
2023-03-18 14:35:19 +0200

    Create authentication helper methods; Add login, logout and signup buttons for header

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to articles_path, notice: 'Welcome to Alpha Blog, you are successfully created'
     else
       render :new, status: :unprocessable_entity
