84ea56af51705d88385e1e988c1dee2c91b6eee7
vlezya <vlezyashop@gmail.com>
2023-03-27 16:08:50 +0300

    Added edit and update actions;Edit category views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,13 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :edit, :update]
   before_action :require_admin, except: [:index, :show]
-
-  def new
-    @category = Category.new
-  end
 
   def index
     @categories = Category.paginate(page: params[:page], per_page: 3)
+  end
+
+  def new
+    @category = Category.new
   end
 
   def create
@@ -14,13 +15,22 @@ class CategoriesController < ApplicationController
     if @category.save
       redirect_to @category, notice: 'Category created successfuly'
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
   def show
-    @category = Category.find(params[:id])
     @articles = @category.articles.paginate(page: params[:page], per_page: 2)
+  end
+
+  def edit; end
+
+  def update
+    if @category.update(category_params)
+      redirect_to categories_path, notice: 'Category name updated successfuly'
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
@@ -33,5 +43,9 @@ class CategoriesController < ApplicationController
     unless logged_in? && current_user.admin?
       redirect_to categories_path, alert: 'You are not authorized to do so'
     end
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
   end
 end
